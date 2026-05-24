ea0e5255629800281fd044153d3df7e3462e66cf
SyauQillah173 <abdullahsyauqillah02@gmail.com>
2026-05-24 23:55:05 +0700

    Expose Vercel runtime errors in debug mode

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+try {
+    require __DIR__.'/../public/index.php';
+} catch (Throwable $exception) {
+    error_log((string) $exception);
+
+    http_response_code(500);
+
+    if (filter_var($_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL)) {
+        header('Content-Type: text/plain; charset=UTF-8');
+
+        echo $exception::class.PHP_EOL;
+        echo $exception->getMessage().PHP_EOL.PHP_EOL;
+        echo $exception->getTraceAsString();
+
+        return;
+    }
+
+    echo 'Server error.';
+}
