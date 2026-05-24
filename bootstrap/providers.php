c19a4b659fd2337e873ab91535591880253e16f3
SyauQillah173 <abdullahsyauqillah02@gmail.com>
2026-05-24 23:36:26 +0700

    Register view provider for Vercel runtime

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
+use Illuminate\View\ViewServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    ViewServiceProvider::class,
 ];
