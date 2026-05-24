10a71c00cb8e80ec5fa53c984abdd9d419b8edb8
SyauQillah173 <abdullahsyauqillah02@gmail.com>
2026-05-25 00:06:03 +0700

    Use writable Vercel cache paths

--- api/index.php
+++ api/index.php
@@ -1,6 +1,34 @@
 <?php
 
 try {
+    if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+        $cacheDirectory = '/tmp/laravel-cache';
+        $viewDirectory = $cacheDirectory.'/views';
+
+        if (! is_dir($cacheDirectory)) {
+            mkdir($cacheDirectory, 0777, true);
+        }
+
+        if (! is_dir($viewDirectory)) {
+            mkdir($viewDirectory, 0777, true);
+        }
+
+        $cachePaths = [
+            'APP_PACKAGES_CACHE' => $cacheDirectory.'/packages.php',
+            'APP_SERVICES_CACHE' => $cacheDirectory.'/services.php',
+            'APP_CONFIG_CACHE' => $cacheDirectory.'/config.php',
+            'APP_ROUTES_CACHE' => $cacheDirectory.'/routes.php',
+            'APP_EVENTS_CACHE' => $cacheDirectory.'/events.php',
+            'VIEW_COMPILED_PATH' => $viewDirectory,
+        ];
+
+        foreach ($cachePaths as $key => $value) {
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+            putenv($key.'='.$value);
+        }
+    }
+
     require __DIR__.'/../public/index.php';
 } catch (Throwable $exception) {
     error_log((string) $exception);
